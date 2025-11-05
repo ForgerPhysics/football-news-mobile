90ebc222bcfad4a0e47cfe05d722fbc86de56588
ForgerPhysics <sihalohoryan2@gmail.com>
2025-11-05 14:53:36 +0700

    tutorial 7 selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:football_news/menu.dart';
+import 'package:football_news/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,10 +29,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue).copyWith(secondary: Colors.blueAccent),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ).copyWith(secondary: Colors.blueAccent),
       ),
       home: MyHomePage(),
     );
   }
 }
-
